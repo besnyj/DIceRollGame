df9c53e1a99454b9628fff94a63388501cf5d554
Felipe Nebesnyj <140928797+besnyj@users.noreply.github.com>
2024-04-02 11:57:32 -0300

    Add files via upload

---
+++ DiceRollGame/UserCommunication/ConsoleReader.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DiceRollGame.UserCommunication
+{
+    public static class ConsoleReader
+    {
+
+        public static int ReadInteger(string message)
+        {
+            int result;
+            do
+            {
+                Console.WriteLine(message);
+            } while (!int.TryParse(Console.ReadLine(), out result));
+
+            return result;
+        }
+    }
+}
